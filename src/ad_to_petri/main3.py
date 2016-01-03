413dc15d9b652ac19a85bb694d7f7df7a53cb0f8
cumtdingkai <cumtdingkai@gmail.com>
2016-01-03 21:29:29 +0100

    main3

--- src/ad_to_petri/main3.py
+++ src/ad_to_petri/main3.py
@@ -4,16 +4,22 @@ Created on 21.12.2015
 @author: Kai
 '''
 from epf import epf_model
-
+from ad_to_petri import draw
+from ad_to_petri import get_reachability_graph
 
 model = epf_model.Model()
 
-#xml_path = "C:/Users/Kai/Desktop/ErrorPro3.4/epf/examples/abc.xml"
-xml_path = "C:/Users/Kai/Desktop/test_parallel_epf.xml"
+# xml_path = "C:/Users/Kai/Desktop/ErrorPro3.4/epf/examples/abc.xml"
+xml_path = "C:/Users/Kai/Desktop/test_exp_epf.xml"
 model.xml.load(xml_path)
 
-model.cf_parallel.get_parallel_process()
-#print model.elements  # key = element name
-#print model.data  # key = data name
-#print model.cf_probs  # key =  "(from,to)", value = prob
-#print model.subsystems  # key = host, value = {intial_element, elements, data}
+parallels = model.cf_parallel.get_parallel_process()    
+index = 0 
+draw_tool = draw.DrawTool()
+rg_tool = get_reachability_graph.PetriToReachabilityGraph()
+for parallel in parallels:
+    index += 1
+    petri_net = model.cf_parallel.get_petri_net(parallel, "parallel" + str(index))
+    rg =rg_tool.get_reachability_graph(petri_net)
+    draw_tool.draw_petri_net(petri_net)
+    draw_tool.draw_reachability_graph(rg)
